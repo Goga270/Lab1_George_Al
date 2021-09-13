0411a3435d3bf85ff14e075da118995b72304e53
gosha <gosha.aladin@gmail.com>
2021-09-13 08:59:10 +0300

    ааа

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,37 @@
 package com.company;//
-public class Main {
 
+import javax.swing.*;
+import java.awt.*;
+
+public class Main extends JFrame {
+    JPanel[] pnl = new JPanel[12];
+
+    public Main(){
+        setLayout(new GridLayout(3,4));
+        for(int i =0;i<pnl.length;i++){
+            int r = (int) (Math.random()*255);
+            int b = (int) (Math.random()*255);
+            int g = (int) (Math.random()*255);
+            pnl[i] = new JPanel();
+            pnl[i].setBackground(new Color(r,g,b));
+            add(pnl[i]);
+        }
+        pnl[4].setLayout(new BorderLayout());
+        pnl[4].add(new JButton("one"),BorderLayout.WEST);
+        pnl[4].add(new JButton("Two"),BorderLayout.SOUTH);
+        pnl[4].add(new JButton("Three"),BorderLayout.NORTH);
+        pnl[4].add(new JButton("Four"),BorderLayout.EAST);
+        pnl[4].add(new JButton("Five"),BorderLayout.CENTER);
+        pnl[10].setLayout(new FlowLayout());
+        pnl[10].add(new JButton("one"));
+        pnl[10].add(new JButton("twoo"));
+        pnl[10].add(new JButton("threee"));
+        pnl[10].add(new JButton("fourrrr"));
+        pnl[10].add(new JButton("Fiveeeee"));
+        setSize(1000,1000);
+        setVisible(true);
+    }
     public static void main(String[] args) {
-	    byte B=5;// число от -127 до 127
-
-        //System.out.println("Число типа byte = " + B);
+        new Main();
     }
 }
